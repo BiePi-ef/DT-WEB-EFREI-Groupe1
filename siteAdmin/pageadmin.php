b909e1396180590aedd8dc065eaa901ebf7df2f2
Cedric HU <cedric.hu@efrei.com>
2025-01-17 15:09:16 +0100

    ajout dans pageadmin.css

--- siteAdmin/pageadmin.php
+++ siteAdmin/pageadmin.php
@@ -8,10 +8,10 @@
         <div id="userfeed">
             <?php foreach ($users as $user){ ?>
                 <div class="user">
-                <div class="float">
+                <div class="float"> 
                     <h3><?php echo htmlspecialchars($user['user_name'])?></h3>
                     <p>Date: <?php echo htmlspecialchars($user['date_create']); ?></p>
-                    <input type="submit" name="deleteuser" value="Supprimer l'utilisateur">
+                    <input type="submit" name="deleteuser" id = "boutton" value="Supprimer l'utilisateur">
                 </div>
             <?php } ?>
         </div>
